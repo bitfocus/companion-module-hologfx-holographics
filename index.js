99060c46ba4a40868601584c2a5c0a94c7bdb463
Floris Porro <floris@streammyevent.com>
2019-07-25 20:04:56 +0200

    Add feedback and presets

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const instance_skel = require('../../instance_skel')
 
-const actionUI = require('./actionUI')
-const actionMethods = require('./actionMethods')
+const actionUI = require('./lib/actionUI')
+const actionMethods = require('./lib/actionMethods')
+const feedback = require('./lib/feedback')
+const presets = require('./lib/presets')
 
 const sdk = require('holographics-client-sdk')
 
@@ -10,7 +12,9 @@ class instance extends instance_skel {
     super(system, id, config);
     Object.assign(this, {
       ...actionUI,
-      ...actionMethods
+      ...actionMethods,
+      ...feedback,
+      ...presets
     })
 
     this.system.emit('instance_actions', this.id, this.getActions())
@@ -68,8 +72,33 @@ class instance extends instance_skel {
     let state = await this.Holographics.state.get()
     let themes = await this.Holographics.themes.find()
     let entries = await this.Holographics.entries.find({ data: {} })
-    
-    this.system.emit('instance_actions', this.id, this.getActions({ state, themes, entries }))
+
+    this.STATE = state
+
+    this.WIDGETS = []
+    if (state) {
+      state.widgets.map((widget) => {
+        this.WIDGETS.push({ id: widget.id, label: widget.name !== '' ? widget.name : widget.type })
+      })
+    }
+    this.THEMES = []
+    if (themes) {
+      themes.map((theme) => {
+        this.THEMES.push({ id: theme.filename, label: theme.filename })
+      })
+    }
+    this.ENTRIES = []
+    if (entries) {
+      entries.map((entry) => {
+        this.ENTRIES.push({ id: entry.id, label: Object.values(entry.props)[0] })
+      })
+    }
+
+    this.system.emit('instance_actions', this.id, this.getActions())
+    this.initPresets()
+    this.initFeedbacks()
+    this.checkFeedbacks('widget_active')
+    this.checkFeedbacks('entry_active')
   }
 
   destroy () {
