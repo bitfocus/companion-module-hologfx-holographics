3e88d8dd4abba7b99a4b6e4fdc96328a07b54fc3
florisporro <Floris@florisporro.com>
2019-07-25 22:56:02 +0200

    Added linter

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const sdk = require('holographics-client-sdk')
 
 class instance extends instance_skel {
 	constructor (system, id, config) {
-		super(system, id, config);
+		super(system, id, config)
 		Object.assign(this, {
 			...actionUI,
 			...actionMethods,
@@ -37,25 +37,25 @@ class instance extends instance_skel {
 			this.Holographics = new sdk.Socket(`http://${this.config.host}:${this.config.port}`)
 
 			// Make sure we only have a single event listener
-			this.Holographics.socket.removeAllListeners('connect');
-			this.Holographics.socket.removeAllListeners('disconnect');
-			this.Holographics.socket.removeAllListeners('stateChanged');
+			this.Holographics.socket.removeAllListeners('connect')
+			this.Holographics.socket.removeAllListeners('disconnect')
+			this.Holographics.socket.removeAllListeners('stateChanged')
 
 			// When the Holographics state changes, make sure we update things relevant to our actions
-			this.Holographics.socket.on('stateChanged', (state) => {
+			this.Holographics.socket.on('stateChanged', () => {
 				this.updateHoloState()
 			})
 			
 			// Show ready status when connected
 			this.Holographics.socket.on('connect', () => {
-				this.log("info", "Connected to Holographics")
+				this.log('info', 'Connected to Holographics')
 				this.status(this.STATUS_OK, 'Ready')
-			});
+			})
 
 			// Show disconnect message when disconnected
 			this.Holographics.socket.on('disconnect', () => {
 				this.handleConnectionError()
-			});
+			})
 
 			// If we're not connected on first setup, show a different warning
 			if (!this.Holographics.socket.connected) this.status(this.STATUS_WARNING, 'Could not connect')
@@ -103,14 +103,14 @@ class instance extends instance_skel {
 
 	destroy () {
 		// Prevent error messages on destroy
-		this.Holographics.socket.removeAllListeners('connect');
-		this.Holographics.socket.removeAllListeners('disconnect');
-		this.Holographics.socket.removeAllListeners('stateChanged');
+		this.Holographics.socket.removeAllListeners('connect')
+		this.Holographics.socket.removeAllListeners('disconnect')
+		this.Holographics.socket.removeAllListeners('stateChanged')
 		// Close the socket
 		this.Holographics.socket.close()
 		delete this.Holographics
 		// Log
-		this.debug("destroy", this.id);;
+		this.debug('destroy', this.id)
 	}
 
 	config_fields () {
@@ -133,7 +133,7 @@ class instance extends instance_skel {
 	}
 
 	handleConnectionError() {
-		this.log('error', "Holographics connection lost")
+		this.log('error', 'Holographics connection lost')
 		this.status(this.STATUS_ERROR, 'Connection error')
 	}
 
@@ -141,7 +141,7 @@ class instance extends instance_skel {
 		if (error.code === 'ECONNREFUSED') {
 			return this.handleConnectionError()
 		} else {
-			this.log('error', e.message)
+			this.log('error', error.message)
 			this.debug(error)
 		}
 	}
@@ -149,10 +149,10 @@ class instance extends instance_skel {
 	action({ action, options }) {
 		if (this.Holographics.socket.connected) {
 			this[action](options)
-				.then((res) => { this.status(this.STATUS_OK, 'Ready') })
+				.then(() => { this.status(this.STATUS_OK, 'Ready') })
 				.catch((e) => { this.handleError(e) })
 		} else {
-			this.log('error', "Not connected to Holographics")
+			this.log('error', 'Not connected to Holographics')
 		}
 	}
 }
